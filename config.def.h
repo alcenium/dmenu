ff8afbcf4e0393108591cec8e0ec3294e64d881b
Alcenium <vuahung0987@gmail.com>
2024-06-23 21:04:26 +0700

    Added vi mode for better navigation and avoid having to add japanese support

--- config.def.h
+++ config.def.h
@@ -23,6 +23,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSelHighlight] =  { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] =           { "#000000", "#00ffff" },
+	[SchemeCursor] =        { "#222222", "#bbbbbb"},
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
@@ -40,3 +41,15 @@ static unsigned int columns    = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * -vi option; if nonzero, vi mode is always enabled and can be
+ * accessed with the global_esc keysym + mod mask
+ */
+static unsigned int vi_mode = 1;
+static unsigned int start_mode = 0;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
+static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
+static Key quit_keys[] = {
+	/* keysym	modifier */
+	{ XK_q,		0 }
+};
